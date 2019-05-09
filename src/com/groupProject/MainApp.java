fa18496ae14a1c97cbac71cfc1295aa3b8037ea7
Luke Haynes <lukehaynes@Lukes-MacBook-Air.local>
2019-05-09 15:37:05 -0400

    Changes

--- src/com/groupProject/MainApp.java
+++ src/com/groupProject/MainApp.java
@@ -3,7 +3,7 @@ package com.groupProject;
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 		System.out.println("Hello World!"); // testing
 
